2af4fd6cad19876ab8f6bb3bd63d0de0754193c4
edwincodee <edwiinnemmanuell@gmail.com>
2025-09-26 19:22:41 +0100

    updated footer layout

--- components/layout/Footer.tsx
+++ components/layout/Footer.tsx
@@ -1,4 +1,84 @@
+import Link from "next/link";
+import {
+  MdOutlineCopyright,
+  MdOutlineEmail,
+  MdOutlineFacebook,
+  MdOutlineMyLocation,
+  MdOutlinePhone,
+} from "react-icons/md";
+
 const Footer: React.FC = () => {
-  return <div className="py-5">footer</div>;
+  return (
+    <div className="bg-gray-100">
+      <div className="flex justify-around py-10">
+        <div>
+          <h3 className="font-bold text-lg">Help & Support</h3>
+
+          <ul className="flex flex-col gap-5 tracking-wide text-sm pt-5">
+            <li>
+              <MdOutlineMyLocation className="text-blue-500" size={15} />
+              <p>674 Shinning Star street, Maitama, Abuja, Nigeria</p>
+            </li>
+            <li>
+              <MdOutlinePhone className="text-blue-500" size={15} />
+              <p>+2340000000000, +234111111111</p>
+            </li>
+            <li>
+              <MdOutlineEmail className="text-blue-500" size={15} />
+              supportt@eddystore.com
+            </li>
+          </ul>
+        </div>
+
+        <div>
+          <h3 className="font-bold text-lg py-">Account</h3>
+
+          <ul className="flex flex-col gap-5 pt-5">
+            <li>
+              <Link href={"/"}>Login / Register</Link>
+            </li>
+            <li>
+              <Link href={"/"}>Cart</Link>
+            </li>
+            <li>
+              <Link href={"/"}>Shop</Link>
+            </li>
+          </ul>
+        </div>
+
+        <div>
+          <h3 className="font-bold text-lg">Quick Link</h3>
+
+          <ul className="flex flex-col gap-5  pt-5">
+            <li>
+              <Link href={"/"}>Privacy Policy</Link>
+            </li>
+            <li>
+              <Link href={"/"}>Refund Policy</Link>
+            </li>
+            <li>
+              <Link href={"/"}>Term of Use</Link>
+            </li>
+          </ul>
+        </div>
+      </div>
+
+      <div className="flex justify-around items-center bg-gray-200 py-1">
+        <div>
+          <MdOutlineCopyright className="inline" />
+          <span className="text-gray-600 px-1">
+            2025. All rights reserved by EddyStore
+          </span>
+        </div>
+
+        <div className="flex items-center text-blue-500 gap-x-1">
+          <p className="italic ">Follow us</p>
+          <Link href="https://facebook.com">
+            <MdOutlineFacebook />
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
 };
 export default Footer;
