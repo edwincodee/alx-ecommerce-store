6763e42a53e8f533983d349e414bebb008522f7b
edwincodee <edwiinnemmanuell@gmail.com>
2025-09-29 00:07:18 +0100

    used fetch instead of axios

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,39 +4,44 @@ import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { AppDispatch, RootState } from "@/store/store";
 import { setProducts } from "@/store/productSlice";
-// import { Products, ProductState } from "@/interfaces";
+import { Products, ProductState } from "@/interfaces";
 import Card from "@/components/common/Card";
+import { GetServerSideProps } from "next";
 
-export default function Home() {
+interface HomeProps {
+  products: Products[];
+}
+
+export default function Home({ products }: ProductState) {
   const dispatch = useDispatch<AppDispatch>();
-  const items = useSelector((state: RootState) => state.products.products);
+  const items = useSelector((state: RootState) => state.products);
   const { category, search } = useSelector(
     (state: RootState) => state.filtered
   );
-  useEffect(() => {
-    const fetchProducts = async () => {
-      try {
-        // call api
-        const res = await axios.get("/api/products");
-        dispatch(setProducts(res.data));
-      } catch (err) {
-        console.error("Failed to fetch products:", err);
-      }
-    };
-    fetchProducts();
-  }, [dispatch]);
-
   // useEffect(() => {
-  //   if (items.products.length > 0) {
-  //     dispatch(setProducts(products));
-  //     // console.log(products);
-  //   }
-  // }, [dispatch, products, items.products.length]);
+  //   const fetchProducts = async () => {
+  //     try {
+  // call api
+  //       const res = await axios.get("/api/products");
+  //       dispatch(setProducts(res.data));
+  //     } catch (err) {
+  //       console.error("Failed to fetch products:", err);
+  //     }
+  //   };
+  //   fetchProducts();
+  // }, [dispatch]);
 
-  const matchedCategory = items.filter((product) => {
+  useEffect(() => {
+    if (items.products.length > 0) {
+      dispatch(setProducts(products));
+      // console.log(products);
+    }
+  }, [dispatch, products, items.products.length]);
+
+  const matchedCategory = products.filter((product) => {
     return product.category.toLowerCase() == category.toLowerCase();
   });
-  const matchedSearch = items.filter((product) => {
+  const matchedSearch = products.filter((product) => {
     return product.title.toLowerCase().includes(search.toLowerCase());
   });
   return (
@@ -47,7 +52,7 @@ export default function Home() {
 
       <div className="grid md:grid-cols-3 sm:grid-cols-2 gap-10 w-[90%] m-auto">
         {search == "" && category.toLowerCase() == "all"
-          ? items.map((product) => (
+          ? products.map((product) => (
               // display all product
               <Card key={product.id} product={product} />
             ))
@@ -70,31 +75,32 @@ export default function Home() {
 }
 
 // fetch product with axios using server side
-// export const getServerSideProps: GetServerSideProps = async () => {
-//   try {
-//     const res = await axios.get("https://fakestoreapi.com/products");
-//     const products = (await res.data) as Products[];
-//     return {
-//       props: {
-//         products,
-//       },
-//     };
-//   } catch (error: unknown) {
-//     let message: string = "An unknown error occured";
-//     if (axios.isAxiosError(error)) {
-//       message = error.message;
+export const getServerSideProps: GetServerSideProps = async () => {
+  try {
+    const res = await fetch("https://fakestoreapi.com/products");
+    const products = (await res.json()) as Products[];
+    return {
+      props: {
+        products,
+      },
+    };
+  } catch (error: unknown) {
+    const message: string = "An unknown error occured";
+    console.log(message, error);
+    // if (axios.isAxiosError(error)) {
+    //   message = error.message;
 
-//       console.error("Axios Fetch Error:", error.message);
-//     } else if (error instanceof Error) {
-//       message = error.message;
-//       console.log(message);
-//     }
+    //   console.error("Axios Fetch Error:", error.message);
+    // } else if (error instanceof Error) {
+    //   message = error.message;
+    //   console.log(message);
+    // }
 
-//     return {
-//       props: {
-//         products: [],
-//         error: "Unable to load products right now",
-//       },
-//     };
-//   }
-// };
+    return {
+      props: {
+        products: [],
+        error: "Unable to load products right now",
+      },
+    };
+  }
+};
